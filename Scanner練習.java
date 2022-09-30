68f7f9de07ae0256ba6ac3fd9362266eba09dbe2
blast66 <113650618+blast66@users.noreply.github.com>
2022-09-30 10:43:08 +0800

    Add files via upload

---
+++ Scanner練習.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class test {
+    public static void main(String []args) {
+		int num1;
+		int num2;
+		int sum;
+		
+		Scanner intput = new Scanner(System.in);
+		System.out.println("ENTER YOU NUM1");
+		num1 = intput.nextInt();
+		
+		System.out.println("ENTER YOU NUM1");
+		num2 = intput.nextInt();
+		
+		sum = num1 + num2;
+		System.out.println("sum is " +sum);
+	}
+	
+
+
+}
